813fd820952730888639955be47900b1a8dc1ca6
Yannick Houbrix <yannick.houbrix@gmail.com>
2018-01-14 19:23:06 +0100

    Basic POC turn LED on and off

--- StatusUfo.ino
+++ StatusUfo.ino
@@ -13,14 +13,16 @@
 #define MQTT_USERNAME    ""
 #define MQTT_PASSWORD    ""
 
+#define LED_PIN 5
+
 Ticker ticker;
 
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_PASSWORD);
 Adafruit_MQTT_Subscribe testFeed = Adafruit_MQTT_Subscribe(&mqtt, "/feeds/test");
 
-void blinkStatusLed(){
-  digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+void blinkStatusLed() {
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
@@ -37,53 +39,57 @@ void MQTT_connect() {
 
   uint8_t retries = 3;
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 10 seconds...");
-       mqtt.disconnect();
-       delay(10000);  // wait 10 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 10 seconds...");
+    mqtt.disconnect();
+    delay(10000);  // wait 10 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
   }
   Serial.println("MQTT Connected!");
 }
 
-void callback(char *data, uint16_t len) {
-  Serial.println(data);
+void callback(uint32_t status) {
+  Serial.println(status);
+  digitalWrite(LED_PIN, status);
 }
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
   digitalWrite(LED_BUILTIN, LOW);
-  ticker.attach_ms(500,blinkStatusLed);
+  ticker.attach_ms(500, blinkStatusLed);
   delay(10);
   Serial.begin(115200);
   delay(10);
-     
+
   WiFiManager wifiManager;
-  wifiManager.resetSettings();
+  //wifiManager.resetSettings();
   wifiManager.autoConnect();
 
   Serial.println("WiFi connected");
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
 
-  ticker.detach();  
+  ticker.detach();
   ticker.attach_ms(100, blinkStatusLed);
 
   testFeed.setCallback(callback);
   mqtt.subscribe(&testFeed);
- 
+
   delay(1000);
   ticker.detach();
+  delay(10);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() {   
-    MQTT_connect();
-    mqtt.processPackets(10000);
-    if(! mqtt.ping()) {
+void loop() {
+  MQTT_connect();
+  mqtt.processPackets(10000);
+  if (! mqtt.ping()) {
     mqtt.disconnect();
   }
 }
--- testbroker.js
+++ testbroker.js
@@ -1,15 +1,25 @@
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://server.home.codeandglory.be')
 
+var express = require('express');
+var app = express();
+
 client.on('connect', function () {
   client.subscribe('/feeds/test')
-
-  setInterval(() => {
-      client.publish('/feeds/test', 'Hello')
-  }, 1000);
-
+  app.listen(3000);
 })
 
 client.on('message', function (topic, message) {
   console.log(message.toString())
 })
+
+
+app.get('/led/:status', function(req, res){
+  let status = 1;
+  if (req.params.status === "off") {
+    status = 0;
+  }
+  console.log("sending", ""+ status)
+  client.publish('/feeds/test', ""+ status);
+  res.status(200).end();
+});
